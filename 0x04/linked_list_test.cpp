9dd754756a94fa6aa2ce8b555643de57a04e748d
Hun-X1 <younghun4218@gmail.com>
2024-06-28 15:57:39 +0900

    링크드리스트 공부

--- 0x04/linked_list_test.cpp
+++ 0x04/linked_list_test.cpp
@@ -5,12 +5,20 @@ const int MX = 1000005;
 int dat[MX], pre[MX], nxt[MX];
 int unused = 1;
 
-void insert(int addr, int num){
-
+void insert(int addr, int num){    
+  
+  dat[unused]=num;
+  pre[unused]=addr;  
+  nxt[unused]=nxt[addr];
+  pre[nxt[addr]]=unused;
+  nxt[addr]=unused;
+  unused++;
 }
 
-void erase(int addr){
-
+void erase(int addr){  
+  
+  pre[nxt[addr]]=pre[addr];
+  nxt[pre[addr]]=nxt[addr];
 }
 
 void traverse(){
